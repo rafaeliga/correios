8e9138b7a829f1d7b8f13ab82419afe8f234cede
Kleber Nascimento Gueriero <kleber.gueriero@gmail.com>
2022-08-18 19:17:20 -0300

    fix: Fix misstyping of variable name

--- lib/auxiliars/helper.rb
+++ lib/auxiliars/helper.rb
@@ -65,7 +65,7 @@ class Helper < CorreiosException
     else
       days.to_i.times do
         date += 1.days
-        date += 1.days if deadline.sunday? || deadline.saturday?
+        date += 1.days if date.on_weekend?
       end
     end
     date
